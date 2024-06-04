96edd7e982fdac55095232a241bf07b02b0f2986
pzmarzly <pawo2500@gmail.com>
2024-06-04 21:17:36 +0100

    xo and bun test

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import {test, expect} from 'bun:test';
+
+test('true', () => {
+	expect(true).toBeTrue();
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export function hi() {
-  console.log("Hello!");
+	console.log('Hello!');
 }
